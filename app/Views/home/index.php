c10721ea59d9eb7d8c17d7a9ba205a3672fc1f19
M. Shahrim <shahrim.private@gmail.com>
2025-07-09 13:55:36 +0800

    designed proper landing page

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -4,14 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Qsystem</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
     <style>
-        body{
-            padding:5em 3em 2em 3em;
-        }
+
     </style>
 </head>
-<body>
-    <h2 style="margin-bottom:0px;">Welcome to Qsystem</h2>
-    <p>click <a href="/login">here</a> to login</p>
+<body class="bg-light">
+    <header class="mb-3">
+        <nav class="navbar bg-white">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <img src="/favicon.ico" alt="Bootstrap" width="30" height="auto" class="d-inline-block align-text-top">
+                    Welcome to Qsystem
+                </a>
+                <a href="/login" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                    login
+                </a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-5 g-4">
+                <div class="col">
+
+                    <!-- RDS -->
+                    <div class="card h-100" style="width: 16rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">RDS</h5>
+                            <p class="card-text">Report Delivery Status for MR and LR</p>
+                        </div>
+                        <div class="card-footer bg-body">
+                            <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col">
+
+                    <!-- eLeave Checker -->
+                    <div class="card h-100" style="width: 16rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">eLeave Checker</h5>
+                            <p class="card-text">check who's on-leave today instantly</p>
+                        </div>
+                        <div class="card-footer bg-body">
+                            <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
