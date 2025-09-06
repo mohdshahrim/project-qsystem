d75e8b17e54073731ea8e194cce5f3101a9ed3bc
mohdshahrim <shahrim.private@gmail.com>
2025-09-06 23:12:34 +0800

    space between shortcuts

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -5,66 +5,107 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Qsystem</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <script src="js/minAjax.js" defer></script>
     <style>
-
+        /* */
     </style>
 </head>
 <body class="bg-light">
-    <header class="mb-3">
+    <header class="mb-3 border-bottom">
         <nav class="navbar bg-white">
             <div class="container">
                 <a class="navbar-brand" href="/">
                     <img src="/favicon.ico" alt="Bootstrap" width="30" height="auto" class="d-inline-block align-text-top">
-                    Welcome to Qsystem
+                    Qsystem
                 </a>
-                <a href="/login" class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                    </svg>
-                    login
-                </a>
+
+                
+                <div>
+                    <!-- temperature display -->
+                    <p id="sibu-temp" class="float-start me-5 text-body-secondary" style="font-size: smaller;"></p>
+
+                    &nbsp;
+
+                    <a href="/login" class="btn btn-primary" role="button">
+                        login
+                    </a>
+                </div>
             </div>
         </nav>
     </header>
     <main>
         <div class="container">
-            <div class="row row-cols-1 row-cols-md-5 g-4">
-                <div class="col">
 
-                    <!-- RDS -->
-                    <div class="card h-100" style="width: 16rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">RDS</h5>
-                            <p class="card-text">Report Delivery Status for MR and LR</p>
-                        </div>
-                        <div class="card-footer bg-body">
-                            <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
-                        </div>
+            <div class="row">
+                
+
+                <!-- RDS -->
+                <div class="card h-100 me-3" style="width: 16rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">RDS</h5>
+                        <p class="card-text">Report Delivery Status for MR and LR</p>
                     </div>
-
-                </div>
-                <div class="col">
-
-                    <!-- eLeave Checker -->
-                    <div class="card h-100" style="width: 16rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">eLeave Checker</h5>
-                            <p class="card-text">check who's on-leave today instantly</p>
-                        </div>
-                        <div class="card-footer bg-body">
-                            <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
-                        </div>
+                    <div class="card-footer bg-body">
+                        <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
                     </div>
-
                 </div>
+
+                <!-- eLeave Checker -->
+                <div class="card h-100 me-3" style="width: 16rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">eLeave Checker</h5>
+                        <p class="card-text">check who's on-leave today instantly</p>
+                    </div>
+                    <div class="card-footer bg-body">
+                        <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
+                    </div>
+                </div>
+
+                <!-- eLeave Checker -->
+                <div class="card h-100 me-3" style="width: 16rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">ClaimMaker</h5>
+                        <p class="card-text">fast and easy way to create claim form</p>
+                    </div>
+                    <div class="card-footer bg-body">
+                        <a href="#" class="btn btn-primary btn-sm card-link align-bottom">Go</a>
+                    </div>
+                </div>
+
             </div>
-
-
 
         </div>
     </main>
     <footer>
 
     </footer>
+
+<script>
+    window.onload = () => {
+        // submit to server for checking
+        minAjax({
+            url: "/getalltemp",
+            type: "GET",
+            success: function(response) {
+                let p_temp = document.getElementById("sibu-temp");
+                var data = JSON.parse(response);
+
+                switch(data["status"]) {
+                    case "OK":
+                        let sibu_temp = data['sibu_temp'];
+                        let kuching_temp = data['kuching_temp'];
+                        p_temp.innerHTML = `😺 Kuching ${kuching_temp}°C &nbsp;&nbsp; 🦢 Sibu ${sibu_temp}°C`;
+                        break;
+                    case "ERROR":
+                        p_temp.innerHTML = "Error";
+                        break;
+                    default:
+                        p_temp.innerHTML = "Error";
+                        break;
+                }
+            }
+        });
+    }
+</script>
 </body>
 </html>
