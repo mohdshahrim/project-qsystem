eb89cee05cfcd8809f509f72f0f2dd56bbf13c5d
M. Shahrim <shahrim.private@gmail.com>
2026-02-03 15:33:42 +0800

    cleanup

--- app/Config/RouteFiles/FragmentRoutes.php
+++ app/Config/RouteFiles/FragmentRoutes.php
@@ -5,4 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/database-management', 'DatabaseManagement::index');
+$routes->get('/fragment', 'Fragment::index');
