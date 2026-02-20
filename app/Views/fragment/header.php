13c53e670b8f483802347332a5158a1f14220edc
M. Shahrim <shahrim.private@gmail.com>
2026-02-20 14:41:19 +0800

    removed main link and set fragment as main

--- app/Views/fragment/header.php
+++ app/Views/fragment/header.php
@@ -7,12 +7,13 @@
 
 
 <?= $this->section('menu') ?>
-    <b>fragment</b>
+    <a href="/fragment" class="w3-button display-block <?php if($navbar=="main"){ echo "w3-asphalt";} else {echo "w3-light-gray";}?>">
+        <b>fragment</b>
+    </a>
 <?= $this->endSection() ?>
 
 
 <?= $this->section('sidebar') ?>
-    <p class="margin-0 <?php if($navbar=="main"){ echo "w3-asphalt";} ?>"><a href="/fragment" class="w3-button display-block">main</a></p>
     <p class="margin-0 <?php if($navbar=="pc"){ echo "w3-asphalt";} ?>"><a href="/fragment/pc" class="w3-button display-block">pc</a></p>
     <p class="margin-0 <?php if($navbar=="monitor"){ echo "w3-asphalt";} ?>"><a href="/fragment/monitor" class="w3-button display-block">monitor</a></p>
     <p class="margin-0 <?php if($navbar=="printer"){ echo "w3-asphalt";} ?>"><a href="/fragment/printer" class="w3-button display-block">printer</a></p>
